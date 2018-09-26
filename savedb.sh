80f234223c4da1f761edec04491e8c4e65996b41
emilie971 <contassot.emilie@gmail.com>
2018-09-26 23:56:58 +0200

    database backup

--- savedb.sh
+++ savedb.sh
@@ -4,7 +4,8 @@ GIT=`which git`
 DATE=$(date)
 REPO_DIR=/home/emilie/these
 echo "Sauvegarde du ${DATE}" > ~/these/logsavedb.log
-mysqldump these these -uemilie -pguadeloupe -h127.0.0.1 > ~/these/dbbackup.sql
+mysqldump these these -uemilie -pguadeloupe -h127.0.0.1 > ~/dbbackup.sql
+sh ~/archivebackup.sh
 echo "OK" >> ~/these/logsavedb.log
 cd ${REPO_DIR}
 ${GIT} commit -am "database backup"

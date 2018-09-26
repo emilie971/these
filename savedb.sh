6f9bc376c7f4f531ab92577c33da24b959a3d3f7
emilie971 <contassot.emilie@gmail.com>
2018-09-27 00:19:49 +0200

    database backup $(date +"%y-%m-%d %I:%M:%S")

--- savedb.sh
+++ savedb.sh
@@ -8,5 +8,5 @@ mysqldump these these -uemilie -pguadeloupe -h127.0.0.1 > ~/dbbackup.sql
 sh ~/archivebackup.sh
 echo "OK" >> ~/these/logsavedb.log
 cd ${REPO_DIR}
-${GIT} commit -am 'database backup $(date +"%y-%m-%d %I:%M:%S)"'
+${GIT} commit -am 'database backup $(date +"%y-%m-%d %I:%M:%S")'
 ${GIT} push origin master

1627e2f6afcdcdbabff85327a5c681961fd6942d
emilie971 <contassot.emilie@gmail.com>
2018-09-27 00:22:03 +0200

    database backup 18-09-27 12:22:03

--- savedb.sh
+++ savedb.sh
@@ -10,4 +10,4 @@ echo "OK" >> ~/these/logsavedb.log
 cd ${REPO_DIR}
 COMMIT_DATE=$(date +"%y-%m-%d %I:%M:%S")
 ${GIT} commit -am "database backup ${COMMIT_DATE}"
-#${GIT} push origin master
+${GIT} push origin master
